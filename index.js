dc1d501282ca366281bc2546109945d09b8eecaa
Oliver Cook <olliewatford@gmail.com>
2020-06-24 20:02:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ function doToElementsInArray(array,callback){
 
 // Add your changeCompletely() function here:
 function changeCompletely(array,index){
-array[index]=`${array[index]}`+2
+array[index]=`${array[index]}`+'2'
 }
