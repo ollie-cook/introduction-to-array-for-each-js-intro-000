e81c48dd2953bea731e306c93d0a44d9b57b6212
Oliver Cook <olliewatford@gmail.com>
2020-06-24 20:04:10 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-// Add your doToElementsInArray() function here:
-
+// Add your doToElementsInArray() function here:fu
+function doToElementsInArray(array,callback){
+  array.forEach(callback)
+}
 
 // Add your changeCompletely() function here:
+function changeCompletely(element,index,array){
+array[index]=`${array[index]}`+'2'
+}
